b0782697488085d39885e9fd4a0ecb70d0c2ebd4
goldfish <27198928@qq.com>
2012-11-05 17:30:55 +0800

    创建项目

---
+++ 移动万网/移动万网/Classes/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  移动万网
+//
+//  Created by Ibokan on 12-11-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ 移动万网/移动万网/Classes/RootViewController.m
@@ -0,0 +1,44 @@
+//
+//  RootViewController.m
+//  移动万网
+//
+//  Created by Ibokan on 12-11-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
